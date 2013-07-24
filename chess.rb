d5fa92d209fb968ca79773a87f9e5ec776b49993
Ed Solis + Ted Cheng <student@appacademy.io>
2013-07-23 18:04:46 -0700

    Pawn works

--- chess.rb
+++ chess.rb
@@ -1,4 +1,6 @@
 class Piece
+  attr_reader :side
+
   def initialize(pos, side, board)
     @pos, @side, @board = pos, side, board
     @on_board = false
@@ -26,23 +28,49 @@ end
 class Pawn < Piece
   def initialize(pos, side, board)
     super(pos, side, board)
-    @first_move = true
   end
 
   def valid_moves
     valid_moves = []
 
     if @side == :black
-      valid_moves << [@pos[0], @pos[1] + 2] if @first_move
-      valid_moves << [@pos[0], @pos[1] + 1]
-      valid_moves << [@pos[0] - 1, @pos[1] + 1] unless board.board[@pos[0] - 1][@pos[1] + 1].nil?
-      valid_moves << [@pos[0] + 1, @pos[1] + 1] unless board.board[@pos[0] + 1][@pos[1] + 1].nil?
-    else
-      valid_moves << [@pos[0], @pos[1] - 2] if @first_move
-      valid_moves << [@pos[0], @pos[1] - 1]
-      valid_moves << [@pos[0] - 1, @pos[1] - 1] unless board.board[@pos[0] - 1][@pos[1] - 1].nil?
-      valid_moves << [@pos[0] + 1, @pos[1] - 1] unless board.board[@pos[0] + 1][@pos[1] - 1].nil?
+      valid_moves << [@pos[0] + 2, @pos[1]] if @pos[0] == 1
+
+      if @board.board[@pos[0] + 1][@pos[1]].nil?
+        valid_moves << [@pos[0] + 1, @pos[1]]
+      end
+
+      # Possible enemy locations
+
+      possible_enemy = @board.board[@pos[0] + 1][@pos[1] - 1]
+      unless possible_enemy.nil? || possible_enemy.side == @side
+        valid_moves << [@pos[0] + 1, @pos[1] - 1]
+      end
+      possible_enemy = @board.board[@pos[0] + 1][@pos[1] + 1]
+      unless possible_enemy.nil? || possible_enemy.side == @side
+        valid_moves << [@pos[0] + 1, @pos[1] + 1]
+      end
+    else # :white
+      valid_moves << [@pos[0] - 2, @pos[1]] if @pos[0] == 6
+      if @board.board[@pos[0] - 1][@pos[1]].nil?
+        valid_moves << [@pos[0] - 1, @pos[1]]
+      end
+
+      # Possible enemy locations
+      possible_enemy = @board.board[@pos[0] - 1][@pos[1] - 1]
+      unless possible_enemy.nil? || possible_enemy.side == @side
+        valid_moves << [@pos[0] - 1, @pos[1] - 1]
+      end
+
+
+      possible_enemy = @board.board[@pos[0] - 1][@pos[1] + 1]
+      unless possible_enemy.nil? || possible_enemy.side == @side
+        valid_moves << [@pos[0] - 1, @pos[1] + 1]
+      end
     end
+    p @side
+    p valid_moves
+    valid_moves
   end
 end
 
@@ -121,20 +149,45 @@ class Board
     puts '  a b c d e f g h'
   end
 
-  def move_piece(from, to)
+  def move_piece(from, to, player)
+    # validate moves, if okay then move
+    #
+    #
+    piece_current_location = @board[from[0]][from[1]]
+    piece_next_location = @board[to[0]][to[1]]
+
+    if piece_current_location.valid_moves.include?(to) && piece_current_location.side == player.side
+
+      #piece_next_location = piece_current_location
+      @board[to[0]][to[1]] = @board[from[0]][from[1]]
+      #piece_current_location = nil
+      @board[from[0]][from[1]] = nil
+    else
+      puts "Invalid move, try again"
+    end
+  end
+
+  # TODO: Make this work
+  def move_piece2(from, to)
     # validate moves, if okay then move
     #
     #
 
+    if get_piece(from[0], from[1]).valid_moves.include?(to)
 
-    @board[to[0]][to[1]] = @board[from[0]][from[1]]
-    @board[from[0]][from[1]] = nil
+      #piece_next_location = piece_current_location
+#      get_piece(to[0], to[1]) = get_piece(from[0], from[1])
+      #piece_current_location = nil
+#      get_piece(from[0], from[1]) = nil
+    else
+      puts "Invalid move, try again"
+    end
   end
 
-#  def get_piece(pos)
-#    a = @board[pos.first][pos.last]
+  def get_piece(x, y)
+    @board[x][y]
 
-#  end
+  end
 
 
 
@@ -171,14 +224,18 @@ class Board
 end
 
 class HumanPlayer
-  def initialize(board)
-    @board = board
+  attr_reader :side
+
+  def initialize(board, side)
+    @board, @side = board, side
   end
 
   def play_turn
     @board.display_board
 
     user_input = self.get_input
+    # If you move a piece opposite your side, u suck
+
     self.move_piece(user_input)
   end
 
@@ -187,6 +244,7 @@ class HumanPlayer
     raw_input = gets.chomp.split(", ")
     from = HumanPlayer.pos_convert(raw_input.first)
     to = HumanPlayer.pos_convert(raw_input.last)
+    p [from, to]
     [from, to]
   end
 
@@ -194,7 +252,7 @@ class HumanPlayer
     from = pos.first
     to = pos.last
 
-    @board.move_piece(from, to)
+    @board.move_piece(from, to, self)
   end
 
   def self.pos_convert(user_pos)
@@ -209,8 +267,8 @@ class ChessGame
 
   def initialize
     @board = Board.new
-    @player1 = HumanPlayer.new(@board)
-    @player2 = HumanPlayer.new(@board)
+    @player1 = HumanPlayer.new(@board, :white)
+    @player2 = HumanPlayer.new(@board, :black)
 
   end
 
